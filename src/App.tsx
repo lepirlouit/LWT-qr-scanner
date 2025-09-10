9ae24bd3ca2a1b10375893c7d6592645b7966479
Benoît de Biolley <benoit@debiolley.be>
2025-09-10 23:53:17 +0200

    ✨ Mui implemenbtation

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,68 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AppBar, Box, Card, CardContent, CardHeader, Toolbar, Typography } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="absolute">
+        <Toolbar
+          sx={{
+            pr: '24px', // keep right padding when drawer closed
+          }}
+        >
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            LWT Scanner
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+
+        <Toolbar />
+        <Box
+          sx={{
+            width: '100%',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+            gap: 2,
+          }}
+        >
+          <Card>
+            <CardHeader title={"LWT Scanner"} />
+            <CardContent>
+              <Typography>WebAssembly Barcode Scanner</Typography>
+              <p className="text-sm text-muted-foreground">Belgish rijksregisternummer Scanner</p>
+            </CardContent>
+          </Card>
+
+          <Card>
+            Hello
+          </Card>
+        </Box>
+      </Box>
+
+    </Box>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#eb002930"
+    },
+    secondary: {
+      main: "#0000ff"
+    }
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
